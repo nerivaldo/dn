8ed57750de266c5b0502f4ba894c4908b74d0dd4
nerivaldo <nerivaldolorenco@hotmail.com>
2024-02-01 09:33:40 +0100

    dn

--- pages/hospedes.js
+++ pages/hospedes.js
@@ -2,22 +2,37 @@
 import React from 'react';
 import hospedes from '/public/hospedes/hospedes.json'
 
+import fs from 'fs';
+import path from 'path';
+
+const filePath = path.resolve(process.cwd(), 'public', 'hospedes.json');
+
+
 const ExibirDados = ({ dados }) => {
   return (
     <div>
-      <h1>Dados do Arquivo JSON</h1>
-      <p>Nome: {dados.NameFormatted}</p>
-    </div>
+      <h1>Dados dos Hospedes</h1>
+      <ul>
+        {dados.data.map((hospede) => (
+          <li key={hospede.RowNumber} className='bg-slate-100 m-1 p-6 border-1 '>
+            <p>ID: {hospede.RowNumber}</p>
+            <p>Nome: {hospede.NameFormatted}</p>
+            <p>Phone1: {hospede.Phone1}</p>
+            <p>BI: {hospede.DocNumber}</p>
+            <p>Data de Nascimento: {hospede.BirthDate}</p>
+          
+          </li>
+        ))}
+      </ul>
+  </div>
   );
 };
 
 export async function getStaticProps() {
-  const filePath = `${process.cwd()}/public/hospedes.json`;
-  console.log('Caminho do arquivo:', filePath);
-
   try {
-    const res = await fetch(filePath);
-    const dados = await res.json();
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const dados = JSON.parse(fileContent);
+    console.log(dados)
 
     return {
       props: {
